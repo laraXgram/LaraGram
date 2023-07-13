70fca25e61b92bfad821cef65a47bfbe5549f675
amirh <amirh.kargar85@gmail.com>
2023-07-13 23:42:35 +0330

    add database migrations

---
+++ Database/Mysql/Migrations/2023_07_13_create_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Bot\Database\Mysql\Migrations;
+
+use Bot\Core\Database\Schema;
+use Illuminate\Database\Schema\Blueprint;
+
+return new class
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
+            $table->string('user_id')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
