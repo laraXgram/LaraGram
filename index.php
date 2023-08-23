0e325d99c68c305811ba451e5e4eafb0d5018de1
amirh <amirh.kargar85@gmail.com>
2023-08-23 21:16:25 +0330

    work on json db eloquent

--- index.php
+++ index.php
@@ -8,3 +8,15 @@ Runner::start();
 
 ?>
 
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title><?php echo $_ENV['APP_NAME']?></title>
+</head>
+<body>
+    <h1 style="font-size: 32px; text-align: center; user-select: none;">Silence is gold!</h1>
+</body>
+</html>
