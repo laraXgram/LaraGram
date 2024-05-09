f1bafaf6ccde10378b6c76fb1853dd7e44def882
amirh <amirh.kargar85@gmail.com>
2024-05-09 21:06:50 +0330

    remove Config folder and move data to .env & and lang folder & change folder structure

---
+++ App/Controller/API/API.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace LaraGram\App\Controller\API;
+
+use LaraGram\Core\Http\RegisterAPI;
+
+class API extends RegisterAPI
+{
+
+}
--- App/Resources/bot.php
+++ App/Resources/bot.php
@@ -1,20 +1,8 @@
 <?php
 
-use Bot\Core\Bot;
-use Bot\Core\Cli\Error\Log;
-use Bot\Core\Cli\Error\Level;
-use Bot\Core\Conversation\Questioner;
-use Bot\Core\Request;
+use LaraGram\Core\Bot;
+use LaraGram\Core\Request;
 
 $bot = new Bot();
 
-
-//$conversation = new Questioner();
-//$conversation->addQuestion("What's your name?")->type('string|required')->name('name');
-//$conversation->addQuestion("What's your phone number?")->type('phonenumber|required')->name('phonenumber');
-//if ($conversation->ask()){
-//    $answers = $conversation->getAnswers();
-//    $data = "name is {$answers['name']} and phone number is {$answers['phonenumber']}";
-//}
-
-//Log::set('test', Level::Error, 'hello this is a test log from bot.php in resources file and this log level is Error. lets to see log result. good bye');
+// Code ...
