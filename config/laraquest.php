230ce221f2d08fc40e12a56a5bad9057f835a3d0
AmirHossein <amirh.kargar85@gmail.com>
2024-10-02 22:11:48 +0330

    feat: update configs files

---
+++ config/laraquest.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    /*
+     * The type of update received.
+     * 1 - sync
+     * 2 - global (default)
+     * 3 - openswoole
+     * (Required)
+     */
+    'update_type' => 'global',
+
+    /*
+     * The Request submission type.
+     * 1 - curl (default)
+     * 2 - no_response_curl
+     * (Required)
+     */
+    'default_mode' => 'curl',
+
+    /*
+     * Long Polling
+     * (Required on polling mode)
+     */
+    'polling' => [
+        'timeout' => 100,
+        'sleep_interval' => 0.5,
+        'limit' => 100,
+        'allow_updates' => ["*"]
+    ],
+];
