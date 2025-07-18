3558b096cf8e25813b3d13952ebc29783bcbf14e
AmirHossein <amirh.kargar85@gmail.com>
2025-07-18 15:31:21 +0330

    refactor: update folder structure to v3

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,7 +7,7 @@ use LaraGram\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withListener(
         bot: __DIR__."/../listens/bot.php",
-        commands: __DIR__."/../listens/commands.php",
+        commands: __DIR__."/../listens/console.php",
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
