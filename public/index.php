d30e06d875dc7a616868a0cad1ce93faf57d0ab7
AmirHossein <amirh.kargar85@gmail.com>
2025-07-27 20:56:05 +0330

    refactor: remove server file and move to core

--- public/index.php
+++ public/index.php
@@ -3,22 +3,9 @@
 $server = escapeshellarg(json_encode($_SERVER));
 $inputs = escapeshellarg(file_get_contents('php://input'));
 
+$vendorDir = realpath(__DIR__ . '/vendor');
+$serverPath = $vendorDir . '/laraxgram/core/src/Foundation/resources/server.php';
+
 $output = '/dev/null'; // You can change it to specifics file.
 
-popen("php ./server {$inputs} {$server} >> {$output} 2>&1 &", "r");
-
-?>
-
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>LaraGram</title>
-</head>
-<body>
-
-</body>
-</html>
+popen("php \"{$serverPath}\" {$inputs} {$server} >> {$output} 2>&1 &", "r");
